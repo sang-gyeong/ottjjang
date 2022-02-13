718348c6af07b69dfbd58b6e35a794433f328148
summer(이상경) <summer.0916@kakaocommerce.com>
2022-02-13 23:01:00 +0900

    feat: httpInterceptor 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {WrapperComponent} from './components/wrapper/wrapper.component';
+import {HttpInterceptorService} from './services/httpInterceptor.service';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, WrapperComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: false}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

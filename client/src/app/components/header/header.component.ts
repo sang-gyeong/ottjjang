ba3059f4bd3fd0af1d668e1c443d5afb5fdc11e8
sang-gyeong <sknglee22@naver.com>
2022-03-22 18:51:15 +0900

    feat: logout 추가

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {createSelector, Store} from '@ngrx/store';
 import {selectUser} from 'src/app/reducers/main.reducer';
 import {LoginService} from 'src/app/services/login.service';
+import {environment} from 'src/environments/environment';
+import {deleteCookie} from 'src/util';
 import * as fromMain from '../../reducers/main.reducer';
 
 const selectVM = createSelector(selectUser, user => {
@@ -27,7 +29,12 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onClickLogin(): any {
+  onClickLogin(): void {
     this.loginService.redirectToLogin();
+  }
+
+  onClickLogout(): void {
+    deleteCookie(environment.auth_token);
+    window.location.href = environment.home_url;
   }
 }
--- client/src/util.ts
+++ client/src/util.ts
@@ -6,3 +6,5 @@ const isMobile = result.device.type === 'mobile';
 const isTablet = result.device.type === 'tablet';
 
 export const isDesktop = () => !isMobile && !isTablet;
+
+export const deleteCookie = (name: string) => (document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;');
